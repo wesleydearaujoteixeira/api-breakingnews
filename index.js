bbad8042407152d7bd45a50d21bcb87e1d45ecd0
Wesley <wesleyaraujoteixeira98@gmail.com>
2024-08-07 21:18:57 -0300

    s

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+// middleware's application 
+
+
+app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+const personal = [
+    {
+        nome: " Wesley",
+        idade:  25,
+        email: "weesley@gmail.com",
+        cidade: "Maracanaú-CE"
+    },
+    {
+        nome: "Renata",
+        idade:  22,
+        email: "renatinha35@gmail.com",
+        cidade: "Ananideua-Pará"
+    },
+
+    {
+        nome: "Junior",
+        idade: 19,
+        email: "juniorTabata23@gmail.com",
+        cidade: "Paraíba-PB"
+    }
+]
+
+
+
+app.get('/news', (req, res) => {
+    res.json(personal);
+});
+
+const PORT = 5000
+
+app.listen(PORT, () => {
+    console.log(`listening on port http://localhost:${PORT}/news`);
 });
